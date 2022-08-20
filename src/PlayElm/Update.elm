06d4e10ab7710f67ab2fb86fc0c0a96521489c23
Andrew Newman <andrewfnewman@gmail.com>
2022-08-21 00:22:54 +1000

    Boot.

--- src/PlayElm/Update.elm
+++ src/PlayElm/Update.elm
@@ -3,6 +3,7 @@ module PlayElm.Update exposing (update)
 import PlayElm.Model as Model
 import PlayElm.Msg as Msg
 import PlayElm.Port as Port
+import PlayElm.Types as Types
 import PlayElm.Util as Util
 import Time as Time
 
@@ -11,9 +12,17 @@ update : Msg.Msg -> Model.Model -> ( Model.Model, Cmd Msg.Msg )
 update msg model =
     case ( msg, model ) of
         ( Msg.Tick newTime, Model.Booting bm ) ->
+            let
+                _ =
+                    Debug.log "Now" "Booting"
+            in
             tick newTime bm |> Tuple.mapFirst Model.Booting
 
         ( Msg.Tick newTime, Model.Running rm ) ->
+            let
+                _ =
+                    Debug.log "Now" "Running"
+            in
             tick newTime rm |> Tuple.mapFirst Model.Running
 
         ( Msg.MouseMove e, Model.Booting bm ) ->
@@ -23,10 +32,10 @@ update msg model =
             mouseMove e.pagePos rm |> Tuple.mapFirst Model.Running
 
         ( Msg.SetBoundingClientRect r, Model.Booting bm ) ->
-            ( Model.Booting { bm | clientRect = r }, Cmd.none )
+            ( boot r bm.computedStyle bm, Cmd.none )
 
         ( Msg.SetComputedStyle s, Model.Booting bm ) ->
-            ( Model.Booting { bm | computedStyle = s }, Cmd.none )
+            ( boot bm.clientRect s bm, Cmd.none )
 
         _ ->
             ( model, Cmd.none )
@@ -40,3 +49,19 @@ tick newTime m =
 mouseMove : ( Float, Float ) -> Model.CommonProperties a -> ( Model.CommonProperties a, Cmd msg )
 mouseMove pos m =
     ( { m | pointer = pos }, Cmd.none )
+
+
+boot : Maybe Types.BoundingClientRect -> Maybe Types.ComputedStyle -> Model.BootingModel -> Model.Model
+boot maybeCr maybeCs m =
+    case ( maybeCr, maybeCs ) of
+        ( Just cr, Just cs ) ->
+            Model.Running { pointer = m.pointer, time = m.time, clientRect = cr, computedStyle = cs }
+
+        ( Just cr, Nothing ) ->
+            Model.Booting { m | clientRect = Just cr }
+
+        ( Nothing, Just cs ) ->
+            Model.Booting { m | computedStyle = Just cs }
+
+        ( Nothing, Nothing ) ->
+            Model.Booting m
